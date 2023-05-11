2bd211bedd33293862d79d7543bdbee0ddc0994e
Romeo Noveanre <noveanrer@gmail.com>
2023-05-11 13:34:15 +0700

    improve minor problems

--- src/config/fb.init.ts
+++ src/config/fb.init.ts
@@ -10,6 +10,17 @@ import { Analytics, getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export default function firebaseInit(): Analytics {
+  // const {
+  //   API_KEY,
+  //   AUTH_DOMAIN,
+  //   PROJECT_ID,
+  //   STORAGE_BUCKET,
+  //   MESSAGE_SENDER_ID,
+  //   APP_ID,
+  //   MEASUREMENT_ID
+  // } = process.env;
+
+
   const firebaseConfig = {
     apiKey: "AIzaSyAvOlPlYlbq8fE51a-f7Ui5AHUlvPBM2qM",
     authDomain: "blazing-base-d3a51.firebaseapp.com",
@@ -18,17 +29,6 @@ export default function firebaseInit(): Analytics {
     messagingSenderId: "798041985405",
     appId: "1:798041985405:web:8a4116a48f73b327f67c14",
     measurementId: "G-4GYR3RR9NZ"
-  };
-
-
-  const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGE_SENDER_ID,
-    appId: APP_ID,
-    measurementId: MEASUREMENT_ID
   };
 
   // Initialize Firebase
